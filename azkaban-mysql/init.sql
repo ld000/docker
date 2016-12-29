5f08cc8b241530cad77bba53c6cc0836eda34560
ld0000 <lidong9144@163.com>
2016-12-29 16:07:09 +0800

    add spl

--- azkaban-mysql/init.sql
+++ azkaban-mysql/init.sql
@@ -207,3 +207,8 @@ CREATE TABLE triggers (
 	data LONGBLOB,
 	PRIMARY KEY (trigger_id)
 );
+
+ALTER TABLE active_executing_flows DROP COLUMN host;
+ALTER TABLE active_executing_flows DROP COLUMN port;
+ALTER TABLE execution_flows ADD COLUMN executor_id INT DEFAULT NULL;
+CREATE INDEX executor_id ON execution_flows(executor_id);
